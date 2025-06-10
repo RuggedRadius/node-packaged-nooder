2ba3219620ba342a78ff2be6a6c68f3ff5775416
Ben Royans <ben.royans@gmail.com>
2025-06-10 10:30:01 +0800

    Glorious initial commit

---
+++ NPN/NPN/Scripts/Test/SpinnyFlag.cs
@@ -0,0 +1,19 @@
+﻿using Stride.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+
+namespace NPN.Scripts.Test
+{
+    public class SpinnyFlag : SyncScript
+    {
+        public override void Update()
+        {
+            var incrementalRotation = Quaternion.RotationYawPitchRoll(0, 0.01f, 0);
+            Entity.Transform.Rotation = incrementalRotation * Entity.Transform.Rotation;
+        }
+    }
+}
