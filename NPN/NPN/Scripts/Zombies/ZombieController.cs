70c50b4287e83f932fa24bfcb2f70a587fe7a48f
JRhys <JRhys@hotmail.co.uk>
2025-06-10 16:02:17 +0800

    JAY-3: Added a zombie.. kind of

---
+++ NPN/NPN/Scripts/Zombies/ZombieController.cs
@@ -0,0 +1,26 @@
+﻿using Stride.Animations;
+using Stride.Engine;
+
+namespace NPN.Scripts.Zombies
+{
+  public class ZombieController : SyncScript
+  {
+    public float Speed = 1.0f;
+
+    private AnimationComponent _animations;
+
+    private PlayingAnimation _currentAnimation;
+
+    public override void Start()
+    {
+      _animations = Entity.Get<AnimationComponent>();
+
+      _currentAnimation = _animations.Play("Walk");
+    }
+
+    public override void Update()
+    {
+
+    }
+  }
+}
