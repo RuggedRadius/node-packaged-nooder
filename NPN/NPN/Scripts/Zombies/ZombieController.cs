550c2b50dbc20b8c92498307ac93123f14782ca0
JRhys <JRhys@hotmail.co.uk>
2025-06-10 22:46:09 +0800

    JAY-8: Added Zombie idle/death animations

--- NPN/NPN/Scripts/Zombies/ZombieController.cs
+++ NPN/NPN/Scripts/Zombies/ZombieController.cs
@@ -1,8 +1,11 @@
 ﻿using Stride.Animations;
 using Stride.Engine;
+using Stride.Input;
 
 namespace NPN.Scripts.Zombies
 {
+
+
   public class ZombieController : SyncScript
   {
     public float Speed = 1.0f;
@@ -11,16 +14,33 @@ namespace NPN.Scripts.Zombies
 
     private PlayingAnimation _currentAnimation;
 
+    public int Health { get; set; } = 100;
+    public bool GamePad { get; set; } = true;
+
     public override void Start()
     {
       _animations = Entity.Get<AnimationComponent>();
 
-      _currentAnimation = _animations.Play("Walk");
+      _currentAnimation = _animations.Play("Idle");
     }
 
     public override void Update()
     {
+      if (GamePad && Input.HasGamePad)
+      {
+        GamePadState gp = Input.DefaultGamePad.State;
 
+        if (gp.LeftTrigger > 0.5)
+        {
+          Health = 0;
+        }
+
+        if (Health <= 0)
+        {
+          if(!_animations.IsPlaying("Dying"))
+            _currentAnimation = _animations.Play("Dying");
+        }
+      }
     }
   }
 }
