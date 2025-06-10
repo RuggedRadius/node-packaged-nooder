623480ec34591c6670d1bf48f326d22334709a78
JRhys <JRhys@hotmail.co.uk>
2025-06-10 16:41:43 +0800

    JAY-5: Added golfer

---
+++ NPN/NPN/Scripts/Golfer/GolferController.cs
@@ -0,0 +1,26 @@
+﻿using Stride.Animations;
+using Stride.Engine;
+
+namespace NPN.Scripts.Golfer
+{
+  public class GolferController : SyncScript
+  {
+    public float Speed = 1.0f;
+
+    private AnimationComponent _animations;
+
+    private PlayingAnimation _currentAnimation;
+
+    public override void Start()
+    {
+      _animations = Entity.Get<AnimationComponent>();
+
+      _currentAnimation = _animations.Play("Drive");
+    }
+
+    public override void Update()
+    {
+
+    }
+  }
+}
